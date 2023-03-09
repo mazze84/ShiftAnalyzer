8b163664d6c9869e24175817ea603e75b7bd39ac
Matthias Wilhelm <matthias.wilhelm@gus-group.com>
2023-03-09 11:04:01 +0100

    Added shift analysis based on speed
    Speed only works outdoors. So shift length should be based on heartrate but for testing purposes it works.
    
    Start new shift if rolling speed is lower than treshhold and time in seconds is long enough

--- Healthfunctions.py
+++ Healthfunctions.py
@@ -14,21 +14,19 @@ def getMinMaxHeartRate(heartrateMap):
             max_heartrate = heartrate
         if heartrate < min_heartrate:
             min_heartrate = heartrate
-    avg = calc_avg_heartrate(avg, cnt)
+    avg = calc_avg(avg, cnt)
     return min_heartrate,max_heartrate,avg
 
-def calc_avg_heartrate(heartrate, cnt):
+def calc_avg(heartrate, cnt, decimals=0):
     avg = 0
     if cnt > 0:
         heartrate /= cnt
-        avg = round(heartrate, 0)
+        avg = round(heartrate, decimals)
     return avg
 
-def calc_timestamps(start_time, end_time):
-    start = datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S.%f%z').timestamp()
-    end = datetime.strptime(end_time, '%Y-%m-%dT%H:%M:%S.%f%z').timestamp()
-
-    return end-start
+def calc_duration_seconds(start_time, end_time):
+    duration = end_time - start_time
+    return duration.total_seconds()
 
 def calc_shift_len(heartrateMap, active_heartrate=100):
     shifts = {}
@@ -50,38 +48,80 @@ def calc_shift_len(heartrateMap, active_heartrate=100):
         # start active shift or bench
         elif last_heartrate < active_heartrate and heartrate > active_heartrate or last_heartrate > active_heartrate and heartrate < active_heartrate:
             # save shift
-            duration_last_shift = calc_timestamps(start_time, time)
-            shifts[shift_cnt] = duration_last_shift, calc_avg_heartrate(avg_heartrate, heartrate_cnt)
+            duration_last_shift = calc_duration_seconds(start_time, time)
+            shifts[shift_cnt] = duration_last_shift, calc_avg(avg_heartrate, heartrate_cnt)
 
             # reset values
             shift_cnt += 1
             start_time = time
             avg_heartrate = heartrate
             heartrate_cnt = 1
-    duration_last_shift = calc_timestamps(start_time, list(heartrateMap.keys())[len(list(heartrateMap.keys()))-1])
-    shifts[shift_cnt] = duration_last_shift, calc_avg_heartrate(avg_heartrate, heartrate_cnt)
+    duration_last_shift = calc_duration_seconds(start_time, list(heartrateMap.keys())[-1])
+    shifts[shift_cnt] = duration_last_shift, calc_avg(avg_heartrate, heartrate_cnt)
     return shifts
 
+def calc_shift_len_speed(trackpoint_map, active_speed=.3, seconds_inactive=5):
+    shifts = {}
+    shift_cnt = 0
 
-def moving_average_exponential(list, x):
+    avg_heartrate = 0
+    heartrate_cnt = 0
+    avg_speed = 0
+    print(trackpoint_map)
+
+    start_time = list(trackpoint_map.keys())[0]
+    last_speed = list(trackpoint_map.values())[0]['Speed_rolling']
+    is_new_speed_active = False
+    for time, trackpoint_list in trackpoint_map.items():
+        heartrate = trackpoint_list['Heartrate']
+        speed = trackpoint_list['Speed_rolling']
+
+        is_old_speed_bench = last_speed <= active_speed
+        is_new_speed_bench = speed <= active_speed
+        is_old_speed_active = last_speed >= active_speed
+        is_new_speed_active = speed >= active_speed
+
+        # continue bench or active shift
+        if (is_old_speed_bench and is_new_speed_bench or is_old_speed_active and is_new_speed_active) or (is_old_speed_bench != is_new_speed_bench or is_old_speed_active != is_new_speed_active) and calc_duration_seconds(start_time, time) < seconds_inactive:
+            avg_heartrate += heartrate
+            heartrate_cnt += 1
+            avg_speed += speed
+        # start active shift or bench
+        else:
+            # save shift
+            duration_last_shift = calc_duration_seconds(start_time, last_time)
+            shifts[shift_cnt] = str(start_time), str(last_time), calc_avg(avg_heartrate, heartrate_cnt), calc_avg(avg_speed, heartrate_cnt, decimals=2), duration_last_shift, is_new_speed_active
+
+            # reset values
+            shift_cnt += 1
+            start_time = time
+            avg_heartrate = heartrate
+            avg_speed = speed
+            heartrate_cnt = 1
+        last_time = time
+        last_speed = speed
+    duration_last_shift = calc_duration_seconds(start_time, list(trackpoint_map.keys())[-1])
+    shifts[shift_cnt] = start_time, list(trackpoint_map.keys())[-1], calc_avg(avg_heartrate, heartrate_cnt), calc_avg(avg_speed, heartrate_cnt, decimals=2), duration_last_shift, not is_new_speed_active
+    return shifts
+
+def moving_average_exponential(list, alpha=0.3, decimals=2):
     numbers_series = pd.Series(list)
 
     # Get the moving averages of series
     # of observations till the current time
-    moving_averages = round(numbers_series.ewm(alpha=0.5, adjust=False).mean(), 0)
+    moving_averages = round(numbers_series.ewm(alpha=alpha, min_periods=1, adjust=False).mean(), decimals)
 
     # Convert pandas series back to list
     return moving_averages.tolist()
 
-def moving_average_cumulative(arr, window_size):
-    window_size = 3
+def moving_average_cumulative(list, window_size):
 
     # Convert array of integers to pandas series
-    numbers_series = pd.Series(arr)
+    numbers_series = pd.Series(list)
 
     # Get the window of series of
     # observations till the current time
-    windows = numbers_series.expanding()
+    windows = numbers_series.expanding(min_periods=1)
 
     # Create a series of moving averages of each window
     moving_averages = windows.mean()
@@ -92,7 +132,7 @@ def moving_average_cumulative(arr, window_size):
 def moving_average(list, rolling_cnt):
     numbers_series = pd.Series(list)
 
-    return numbers_series.rolling(rolling_cnt).mean().tolist()
+    return numbers_series.rolling(rolling_cnt, min_periods=1).mean().tolist()
 
 def rising_edge(data, thresh):
     sign = data >= thresh
@@ -102,3 +142,7 @@ def rising_edge(data, thresh):
 def print_csv(heartrate_map):
     for time, heartrate in heartrate_map.items():
         print(time + ";" + str(heartrate))
+
+# extracts column in matrix to list
+def extract(lst, index):
+    return [item[index] for item in lst]
--- main.py
+++ main.py
@@ -17,17 +17,56 @@ def main():
     df = pd.DataFrame.from_dict(trackpoint_map, orient='index', columns=['Heartrate', 'Speed', 'Distance'])
     df.index.name = "Time"
 
-    df['Heartrate_avg'] = Healthfunctions.moving_average(extract(trackpoint_map.values(), 0), 50)
+    df['Heartrate_avg'] = Healthfunctions.moving_average_exponential(extract(trackpoint_map.values(), 0), alpha=0.1, decimals=0)
     df['Heartrate_avg_rolling'] = Healthfunctions.moving_average(extract(trackpoint_map.values(), 0), 10)
-    df['Speed_rolling'] = Healthfunctions.moving_average(extract(trackpoint_map.values(), 1), 5)
+    df['Speed_rolling'] = Healthfunctions.moving_average(extract(trackpoint_map.values(), 1), 10)
     print(df)
-    #print(avg_dataframe)
-    # Sorting the two columns in ascending order
+    df.to_csv('activity.csv', index=True, sep=';')
+
+    activity_map = df.to_dict('index')
+
+    shifts = Healthfunctions.calc_shift_len_speed(activity_map)
+
+    # print(avg_dataframe)
+    # Sorting the columns in ascending order
     df.sort_values(["Time"], axis=0, inplace=True)
-    #, fdf[''], fdf['']
+    plot_heartrate(df)
+    df_shifts = pd.DataFrame.from_dict(shifts, orient='index', columns=['Starttime', 'Endtime', 'AverageHeartRate', 'AverageSpeed', 'Duration', 'Active'])
+    print(df_shifts)
+
+    #plot_shifts(shifts)
+    plt.show()
+
+
+def plot_shifts(df_shifts):
+    #plt.figure(2)
+
+    plt.plot(df_shifts.index.values, df_shifts['Starttime'])
+    plt.plot(df_shifts.index.values, df_shifts['Endtime'])
+    plt.plot(df_shifts.index.values, df_shifts['AverageHeartRate'])
+    plt.plot(df_shifts.index.values, df_shifts['AverageSpeed'])
+    plt.plot(df_shifts.index.values, df_shifts['Duration'])
+    plt.ylabel("Heartrate")
+    plt.xlabel("Shift")
+
+
+def plot_shift_bar(df_shifts):
+    fig, ax = plt.subplots()
+
+    ax.bar(x, y, width=1, edgecolor="white", linewidth=0.7)
+
+    ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+           ylim=(0, 8), yticks=np.arange(1, 8))
+
+    plt.show()
+
+
+def plot_heartrate(df):
     plt.figure(1)
     plt.subplot(211)
     plt.title('Heartrate')
+    plt.ylabel("Heartrate")
+    plt.xlabel("Time")
     plt.plot(df.index.values, df['Heartrate'])
     plt.plot(df.index.values, df['Heartrate_avg'])
     plt.plot(df.index.values, df['Heartrate_avg_rolling'])
@@ -35,10 +74,10 @@ def main():
     plt.title('Speed')
     plt.plot(df.index.values, df['Speed'])
     plt.plot(df.index.values, df['Speed_rolling'])
-    #plt.plot(df.index.values, df['Distance'])
-    plt.ylabel("Heartrate")
+    # plt.plot(df.index.values, df['Distance'])
+    plt.ylabel("Speed")
     plt.xlabel("Time")
-    plt.show()
+
 
 def import_tcx_file(file):
     tree = xml.dom.minidom.parse(file)
@@ -67,14 +106,12 @@ def import_tcx_file(file):
         trackpoint_map[timestamp] = [heartrate, speed, distance]
     return trackpoint_map
 
+
+# extracts column in matrix to list
 def extract(lst, index):
     return [item[index] for item in lst]
-
-
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
